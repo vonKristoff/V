14d34900965d7310c0b9bbb43cc4ec77f434f1e5
Vonkristoff <mrjcnicolas@gmail.com>
2015-07-18 11:17:32 +0100

    added returns for chaining, and creating better method names, because ofstatic / non-static function issue translation from pVector

--- v.js
+++ v.js
@@ -8,32 +8,55 @@ V.prototype = {
 
   info: function(){
     console.log('x: ' + this.x, 'y: ' + this.y);
+    return this
   },
   clone: function (){
     return new V(this.x, this.y)
+    return this
   },
   feed: function (vector){
     this.x = vector.x;
     this.y = vector.y;
+    return this
   },
   add: function (vector){
     this.x += vector.x;
     this.y += vector.y;
+    return this
   },
   sub: function (vector){
-    this.x = vector.x - this.x;
-    this.y = vector.y - this.y;
+    this.x -= vector.x 
+    this.y -= vector.y
+    return this
   },
-  mult: function (n){
+  scale: function (n){
     this.x *= n;
     this.y *= n;
+    return this
   },
   div: function (n){
     this.x /= n;
     this.y /= n;
+    return this
+  },
+  create: {
+    add: function (a, b){
+      return new V( a.x + b.x, a.y + b.y )
+    },
+    sub: function (a, b){
+      return new V( a.x - b.x, a.y - b.y )
+    },
+    mult: function (n){
+      
+    
+    },
+    div: function (n){
+      
+    }
   },
   mag: function (){
     return Math.sqrt( (this.x * this.x) + (this.y * this.y) )
+    return this
   },
   dir: function (target){
     var current = this.clone();
@@ -43,18 +66,22 @@ V.prototype = {
   limit: function (n){
     this.x = (this.x < n) ? this.x : n;
     this.y = (this.y < n) ? this.y : n;
+    return this
   },
   normalise: function (){
     var m = this.mag();
 
     this.x = this.x / m;
     this.y = this.y / m;
+    return this
   },
   dot: function (vector) {
     return (this.x * vector.x) + (this.y * vector.y) ;
+    return this
   },
   reverse: function (){
     this.x = -this.x;
     this.y = -this.y;
+    return this
   }
 }
